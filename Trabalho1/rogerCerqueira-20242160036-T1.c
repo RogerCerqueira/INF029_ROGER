6bc4087a93f6f3b8c114c87974b33cc488af8bac
Roger Cerqueira <rogerdisgracahehe@gmail.com>
2026-07-08 18:34:38 -0300

    Move para pasta Trabalho1

--- Trabalho1/rogerCerqueira-20242160036-T1.c
+++ Trabalho1/rogerCerqueira-20242160036-T1.c
@@ -21,7 +21,7 @@
 // #################################################
 
 #include <stdio.h>
-#include "trabalho.h" 
+#include "trabalho1.h" 
 #include <stdlib.h>
 
 DataQuebrada quebraData(char data[]);
